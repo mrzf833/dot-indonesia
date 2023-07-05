d095e1ec2fc493496ddaab0d7f57ed0f7c6ccac9
mrzf833 <palaktais@gmail.com>
2023-07-05 18:27:17 +0700

    Membuat sumber data pencarian province & cities bisa melalui database​ atau direct API​ raja ongkir (swapable implementation)

--- app/Services/CityService.php
+++ app/Services/CityService.php
@@ -2,11 +2,12 @@
 
 namespace App\Services;
 
+use App\Helpers\HttpRajaongkir;
 use App\Models\City;
 
 class CityService
 {
-    public function search($id = null)
+    public function searchDb($id = null)
     {
         if($id == null){
             $data = City::join('provinces', 'cities.province_id', '=', 'provinces.province_id')
@@ -22,5 +23,38 @@ class CityService
             'results' => $data,
             'code' => 200
         ];
+    }
+
+    public function searchRajaongkir($id = null)
+    {
+        $response = HttpRajaongkir::exec()->get("starter/city", [
+            'id' => $id
+        ]);
+
+        $status = strval($response->status())[0];
+
+        if($status != "2"){
+            return [
+                'message' => $response->json()['rajaongkir']['status']['description'],
+                'code' => $response->json()['rajaongkir']['status']['code'],
+            ];
+        }
+
+        return [
+            'message' => $response->json()['rajaongkir']['status']['description'],
+            'results' => $response->json()['rajaongkir']['results'],
+            'code' => $response->json()['rajaongkir']['status']['code'],
+        ];
+    }
+
+    public function search($id = null)
+    {
+        if(config('rajaongkir')['swap_rajaongkir_or_db'] == "rajaongkir"){
+            $response = $this->searchRajaongkir($id);
+        }else {
+            $response = $this->searchDb($id);
+        }
+
+        return $response;
     }
 }
--- app/Services/ProvinceService.php
+++ app/Services/ProvinceService.php
@@ -2,11 +2,12 @@
 
 namespace App\Services;
 
+use App\Helpers\HttpRajaongkir;
 use App\Models\Province;
 
 class ProvinceService
 {
-    public function search($id = null)
+    public function searchDb($id = null)
     {
         if($id == null){
             $data = Province::all();
@@ -19,5 +20,38 @@ class ProvinceService
             'results' => $data,
             'code' => 200
         ];
+    }
+
+    public function searchRajaongkir($id = null)
+    {
+        $response = HttpRajaongkir::exec()->get("starter/province", [
+            'id' => $id
+        ]);
+
+        $status = strval($response->status())[0];
+
+        if($status != "2"){
+            return [
+                'message' => $response->json()['rajaongkir']['status']['description'],
+                'code' => $response->json()['rajaongkir']['status']['code'],
+            ];
+        }
+
+        return [
+            'message' => $response->json()['rajaongkir']['status']['description'],
+            'results' => $response->json()['rajaongkir']['results'],
+            'code' => $response->json()['rajaongkir']['status']['code'],
+        ];
+    }
+
+    public function search($id = null)
+    {
+        if(config('rajaongkir')['swap_rajaongkir_or_db'] == "rajaongkir"){
+            $response = $this->searchRajaongkir($id);
+        }else {
+            $response = $this->searchDb($id);
+        }
+
+        return $response;
     }
 }
